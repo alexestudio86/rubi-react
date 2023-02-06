7f202acebe3becb5d754f4548b808eb93fe74ae6
Alejandro R <alexestudio86@gmail.com>
2023-02-06 12:22:07 -0600

    carrousel updated

--- src/components/carousel/Carousels.jsx
+++ src/components/carousel/Carousels.jsx
@@ -5,14 +5,17 @@ export function Carousels ( ){
 
     const slides = [
         {
-            slideTitle: "Slide 01",
+            slideTitle: "Mascarillas",
             slideUrl:   require("../../assets/carousel/slide_01.jpeg")
         },{
-            slideTitle: "Slide 02",
+            slideTitle: "En lugar de crema, poner permanente",
             slideUrl:   require("../../assets/carousel/slide_02.jpeg")
         },{
-            slideTitle: "Slide 03",
+            slideTitle: "En lugar de multi, poner líquidos",
             slideUrl:   require("../../assets/carousel/slide_03.jpeg")
+        },{
+            slideTitle: "Shampoo",
+            slideUrl:   require("../../assets/carousel/slide_04.jpeg")
         }
     ]
 
