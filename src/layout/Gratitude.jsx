8f378a1ff94f42fc870687680a6ac3a98bf09f1b
Alejandro R <alexestudio86@gmail.com>
2023-03-14 15:16:31 -0600

    final v 1.0

--- src/layout/Gratitude.jsx
+++ src/layout/Gratitude.jsx
@@ -24,20 +24,16 @@ export function Gratitude ( {state, setState} ){
     const introText = encodeURIComponent(`_Hola, me gustaría ordenar_\n\n`);
 
     // Texto
-    const bodyText = car.lenght > 0 ?
-        car.map( c => (
+    const bodyText = car.map( c => (
             c.variants.map( v => (
                 encodeURIComponent(`${v.quantity} x ${c.name} | ${v.name} - $${v.quantity*v.price}\n`)
             ) )
         ) ).join('')
-        : '';
 
     // Cantidad
-    const dinero = car.lenght > 0 ?
-        car.map( c => (
+    const dinero = car.map( c => (
             c.variants.reduce( (sum, variant) => (typeof variant.quantity === 'number' ? sum + (variant.quantity*variant.price) : sum), 0 )
         ) ).reduce( (accumulator, currentValue) => (accumulator + currentValue) )
-        : 0;
 
     // Divisiòn
     const dineroTotal = encodeURIComponent(`____________________\nTotal: ${dinero}\n`)
@@ -52,9 +48,9 @@ export function Gratitude ( {state, setState} ){
     useEffect(() => {
         const timer = setTimeout(() => {
             updateGuestName('')
+            setState( state => ({ ...state, slide:'car' }) )
             updateCar({actionType: 'CLEAR_ALL'},{})
             window.location.replace(link)
-            setState( state => ({ ...state, slide:'car' }) )
         }, 5000);
         return () => clearTimeout(timer);
     }, []);
